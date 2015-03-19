5daea64ae2d703c201fd0992ac76255b13822859
Ibrahim Abdel Fattah Mohamed <ibrahim.abdelfattah@tedata.net>
2015-03-19 18:14:34 +0200

    Adds a simple UI using bootstrap, font-awesome and Jade template engine

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+// Instantiates express
+var express = require('express')
+
+// Instantiates an application
+var app = express();
+
+var PORT_LISTENER = 3000;
+app.set('port', process.env.PORT || PORT_LISTENER);
+
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get("*", function(req, res){
+    res.render('index')
+});
+
+app.listen(app.get('port'), function(req, res){
+    console.log("Listening on port: " + app.get('port'));
+})
